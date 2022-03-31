5ec0d704474263657e833f8f4e0bc2338c6c228a
Fabien Schneider <f.schneider@cokuna.com>
2022-03-31 15:43:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from sys import exit
+import os
 import SPRNVA as sprnva
+import time
 from SPRNVA import Vector
 pygame.init()
 
@@ -23,20 +25,23 @@ class Main:
 
         # Button setup and tile type selection
         self.tile_select_b_size = Vector(50, 50)
-        self.tile_types = {'1': 'grass', '2': 'stone', '3': 'wood'}
+        self.tile_types = {'1': 'NONE / Air', '2': 'stone', '3': 'wood'}
         self.current_selected_tile_type = '1'
 
         # Stores the 'Drawn' tiles in a list
         self.tiles = []
         self.tile_m_x = 0
         self.tile_m_y = 0
+        self.selected_tile = (0,0)
+
+        # Checks if the user has pressed the export button
+        self.export = False
 
         # Stores the Grid dimensions and tile size in a list
         self.grid_params = {'x': '0', 'y': '0', 'size': '0'}
 
     def update(self):
         grid_surf = pygame.Surface((self.win_size[0]/1.2, self.win_size[1]/1.2))
-        grid_rect = grid_surf.get_rect()
         gen_map = True
 
         grid_param_inputs = {}
@@ -46,6 +51,7 @@ class Main:
         while True:
             # Clears surfaces
             grid_surf.fill((64, 64, 64))
+            grid_rect = grid_surf.get_rect()
             self.win.fill((0, 0, 0))
 
             # Displays fps
@@ -64,6 +70,16 @@ class Main:
                 pygame.quit()
                 print(self.tiles)
                 exit()
+
+            # Sets up the export button
+            ex_button = sprnva.Button(self.win,
+                                           self.win_size[0] - 120,
+                                           self.center.y,
+                                           100,
+                                           50,
+                                           (m_x, m_y),
+                                           m_btns,
+                                           0, btn_text='Export as .lvl')
 
             # loop through available tile types and generate a button at the button of the screen if the button is pressed the active tile type switches, default is the first dictonary entry
             for i, (key, value) in enumerate(self.tile_types.items()):
@@ -101,33 +117,134 @@ class Main:
 
                 pygame.draw.rect(grid_surf, (255, 255, 255), pygame.Rect(self.tile_m_x, self.tile_m_y, int(self.grid_params['size']), int(self.grid_params['size'])))
 
-                for y in range(int(int(self.grid_params['y']) / int(self.grid_params['size']))):
-                    pygame.draw.line(grid_surf, (255, 255, 255), (0, int(self.grid_params['size']) * y), (int(self.grid_params['x']), int(self.grid_params['size']) * y))
-
-                    for x in range(int(int(self.grid_params['x']) / int(self.grid_params['size']))):
-                        pygame.draw.line(grid_surf, (255, 255, 255), (int(self.grid_params['size']) * x, 0), (int(self.grid_params['size']) * x, int(self.grid_params['y'])))
-
-                if self.grid_params['x'] != '0' and self.grid_params['y'] != '0' and self.grid_params['size'] != '0' and gen_map == True:
-                    row = int(self.grid_params['x']/int(self.grid_params['size'])) * ['0']
-                    self.tiles = int(self.grid_params['y']/int(self.grid_params['size'])) * [row]
+                if self.grid_params['x'] != '0' and self.grid_params['y'] != '0' and self.grid_params['size'] != '0' and gen_map is True:
+                    row = int(self.grid_params['x']) * ['0']
+                    self.tiles = int(self.grid_params['y']) * [row]
                     gen_map = False
 
-                if m_btns == (True, False, False):
-                    print(int(self.tile_m_x/int(self.grid_params['size'])), int(self.tile_m_y/int(self.grid_params['size'])))
-                    self.tiles[int(self.tile_m_y/int(self.grid_params['size']))][int(self.tile_m_x/int(self.grid_params['size']))] = self.current_selected_tile_type
+                # Generates grid
+                for y in range(int(self.grid_params['y'])):
+                    if int(self.grid_params['y']) <= grid_rect.height:
+                        pygame.draw.line(grid_surf, (255, 255, 255), (0, y * int(self.grid_params['size'])), (grid_rect.width, y * int(self.grid_params['size'])))
+
+                    for x in range(int(self.grid_params['x'])):
+                        if int(self.grid_params['x']) <= grid_rect.width:
+                            pygame.draw.line(grid_surf, (255, 255, 255), (x * int(self.grid_params['size']), 0), (x * int(self.grid_params['size']), grid_rect.height))
+
+                for y, row in enumerate(self.tiles):
+                    print(row, '\n')
+                    for x, col in enumerate(row):
+                        print(col, '\n')
+                        print(self.tiles[y][x])
+                        if m_btns == (True, False, False):
+                            if grid_rect.collidepoint(m_x, m_y):
+                                self.selected_tile = (x, y)
+
+                if self.grid_params['x'] != '0' and self.grid_params['y'] != '0' and self.grid_params['size'] != '0':
+                    self.export = ex_button.draw()
+
+                #self.tiles[self.selected_tile[1]][self.selected_tile[0]] = self.current_selected_tile_type
 
             except ZeroDivisionError:
                 pass
+
+            if self.export:
+                ex_surf = pygame.Surface((self.win_size[0], self.win_size[1]))
+                ex_surf_rect = ex_surf.get_rect()
+                tb_ex_path = sprnva.InputBox(ex_surf, Vector(20, ex_surf_rect.height / 2 - 10), Vector(ex_surf_rect.width - 40, 20))
+                self.win.fill((0,0,0))
+                while True:
+                    ex_surf.fill((64, 64, 64))
+                    # Displays fps
+                    sprnva.TextRenderer(ex_surf, self.win_size[0] - 150, 50, 'FPS: ' + str(int(self.clock.get_fps())),
+                                        'Arial', 20, (255, 0, 0))
+
+                    events = pygame.event.get()
+
+                    keys = pygame.key.get_pressed()
+                    m_x, m_y = pygame.mouse.get_pos()
+                    m_btns = pygame.mouse.get_pressed()
+
+                    sprnva.TextRenderer(ex_surf,
+                                        ex_surf_rect.centerx,
+                                        ex_surf_rect.centery - ex_surf_rect.height/2 + 40,
+                                        'Enter Export path: ', 'Arial', 20, (255, 255, 255))
+
+                    export_button = sprnva.Button(ex_surf,
+                                  ex_surf_rect.width - 120,
+                                  ex_surf_rect.height - 70,
+                                  100,
+                                  50,
+                                  (m_x, m_y),
+                                  m_btns,
+                                  0, btn_text='Export as .lvl')
+
+                    back_button = sprnva.Button(ex_surf,
+                                                  20,
+                                                  ex_surf_rect.height - 70,
+                                                  100,
+                                                  50,
+                                                  (m_x, m_y),
+                                                  m_btns,
+                                                  0, btn_text='Back')
+
+                    tb_ex_path.get_input(events)
+                    tb_ex_path.update((True, False, False))
+                    tb_ex_path.draw(color=(255, 255, 255), text_color=(0, 0, 0))
+
+                    back_off = back_button.draw()
+                    should_export = export_button.draw()
+
+                    if should_export:
+                        ex_path = tb_ex_path.get_value()
+                        file = open(ex_path + '.lvl', 'w')
+                        lines = []
+
+                        for row in self.tiles:
+                            for char in row:
+                                lines.append(char)
+                            lines.append('\n')
+
+                        print(lines)
+                        file.writelines(lines)
+                        file.close()
+                        pygame.quit()
+                        exit()
+
+                    if back_off:
+                        break
+
+                    for event in events:
+                        if event.type == pygame.QUIT:
+                            pygame.quit()
+                            print(self.tiles)
+                            exit()
+
+                    if keys[pygame.K_ESCAPE]:
+                        pygame.quit()
+                        exit()
+
+                    self.win.blit(ex_surf, (0, 0))
+                    pygame.display.update()
+                    self.clock.tick(self.fps)
 
             # Handel's exit
             for event in events:
                 if event.type == pygame.QUIT:
                     pygame.quit()
-                    print(self.tiles)
                     exit()
 
+            for r_index, row in enumerate(self.tiles):
+                for c_index, char in enumerate(row):
+                    if char == '1':
+                        pygame.draw.rect(grid_surf, (255, 0, 0), pygame.Rect(c_index * int(self.grid_params['size']),
+                                                                             r_index * int(self.grid_params['size']),
+                                                                             int(self.grid_params['size']),
+                                                                             int(self.grid_params['size'])))
+
+
             # Draws, resets the loop and keeps the framerate capped.
-            self.win.blit(grid_surf, (0, 0))#(self.center.x - grid_surf.get_width()/2, self.center.y - grid_surf.get_height()/2))
+            self.win.blit(grid_surf, (0, 0))
             pygame.display.update()
             self.clock.tick(self.fps)
 
